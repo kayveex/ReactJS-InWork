a3b3ebe33e107de5d6024ae60eca33dad4cd2fce
unknown <rhesakornelius@gmail.com>
2023-08-26 10:20:03 +0700

    Fixing Minor Bugs

--- src/partials/Home/Jumbotron.jsx
+++ src/partials/Home/Jumbotron.jsx
@@ -10,8 +10,11 @@ export const Jumbotron = () => {
             <p className="max-w-2xl mb-6 font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               InWork is a leading platform that offers comprehensive insights into job opportunities and companies in the employment landscape. As a job seeker or employer, you'll find valuable resources tailored to your needs.
             </p>
-            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-bold text-center text-white rounded-lg bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:ring-lime-300 dark:focus:ring-lime-900">
-              Get started
+            <a
+              href="/job-list"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-bold text-center text-white rounded-lg bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:ring-lime-300 dark:focus:ring-lime-900"
+            >
+              Job List
               <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
               </svg>
